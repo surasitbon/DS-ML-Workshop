c92089299b7fe397e627c7153557bc73661671e9
surasitbon <surasitbon@gmail.com>
2026-06-03 15:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,5 @@ elif st.button(" 🧹การทำความสะอาดข้อมู�
     st.switch_page("pages/cleaningbyLLVQ.py")
 elif st.button(" 💫 การเแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
+elif st.button(" ☄️ การวิเคราะห์ข้อมูลเชิงสำรวจ"):
+    st.switch_page("pages/EDA_app.py")
