c4b3fc85b862037223e71cd270fb6a5ad303bef3
surasitbon <surasitbon@gmail.com>
2026-06-04 14:11:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,5 @@ elif st.button(" ☄️ การวิเคราะห์ข้อมูล�
     st.switch_page("pages/EDA_app.py")
 elif st.button(" 🛍️ การพยากรณ์ยอดขายแบบง่าย"):
     st.switch_page("pages/sale_predict.py")
+elif st.button(" 🚚 การพยากรณ์ระยะเวลาการให้บริการขนส่ง"):
+    st.switch_page("pages/truck_predict.py")
