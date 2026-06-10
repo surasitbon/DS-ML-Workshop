4f411b6657760e52b6b7b71b6d54d2ed51f594e2
surasitbon <surasitbon@gmail.com>
2026-06-10 13:30:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("""
         padding: 12px;
         font-size: 16px;
         transition: all 0.3s ease;
-        background-color: #CC00FF;
+        background-color: 1E90FF;
         box-shadow: 0 2px 4px rgba(0,0,0,0.05);
     }
     /* เอฟเฟกต์ตอนเอาเมาส์ไปชี้ที่ปุ่ม */
